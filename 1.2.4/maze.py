af5c598546dafbef90f4538c30fbc66480e39297
12481 <riley.orr@vtsd.com>
2024-12-02 13:08:43 -0500

    hi

---
+++ 1.2.4/maze.py
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def draw_barrier
+
+
+
+
+
+
+
+for wall in range(21):
+    maze_painter.foward(length/3)
+    maze_painter.penup()
+    maze_painter.foward(path_width)
+    maze_painter.pendown
